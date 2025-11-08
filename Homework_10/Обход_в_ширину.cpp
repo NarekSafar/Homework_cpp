52a1a74d07604845739ab482747f7fb4ec0d6e2a
NarekSafar <narek.safaryan.06@bk.ru>
2025-11-08 12:35:02 +0400

    binary tree

---
+++ Homework_10/Обход_в_ширину.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+    
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    
+    explicit TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+void levelOrder(TreeNode* root) {
+    if (!root) return; 
+    
+    std::queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size(); 
+        
+        for (int i = 0; i < levelSize; ++i) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            std::cout << node->data << " ";
+
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+    }
+}
+
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    
+    levelOrder(root);
+}
